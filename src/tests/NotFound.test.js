8abf0830ed75469015b85c78d9fed5df3070af97
Paloma Wink <paloma.wink@hotmail.com>
2023-05-02 18:40:05 -0300

    Requisito 4 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('Verifica se a página Not Found apresenta os elementos corretamente', () => {
+  renderWithRouter(<NotFound />);
+  screen.getByRole('heading', {
+    name: /page requested not found/i,
+  });
+  const img = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
