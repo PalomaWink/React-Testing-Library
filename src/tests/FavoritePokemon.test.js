695e275177ef11ef199bccdbb35b5dc7621f9414
Paloma Wink <paloma.wink@hotmail.com>
2023-05-02 18:31:57 -0300

    Requisito 3 concluido

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import pokemonList from '../data';
+
+test('Verifica se aparece apenas os pokemons favoritados', () => {
+  renderWithRouter(<FavoritePokemon pokemonList={ [pokemonList[0]] } />);
+  screen.getByRole('img', {
+    name: /pikachu is marked as favorite/i,
+  });
+
+  renderWithRouter(<FavoritePokemon pokemonList={ [pokemonList[1]] } />);
+  screen.getByRole('img', {
+    name: /charmander is marked as favorite/i,
+  });
+});
+
+test('Verifica se aparece a mensagem "No favorite pokemon found", caso a pessoa não tenha pokemons favoritados', () => {
+  renderWithRouter(<FavoritePokemon />);
+  screen.getByText(/no favorite pokémon found/i);
+});
