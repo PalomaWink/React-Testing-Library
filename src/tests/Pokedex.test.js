935433cc4e2183f4aa4f0667ef0d739fafee0bc2
Paloma Wink <paloma.wink@hotmail.com>
2023-05-03 16:41:32 -0300

    Concluido o requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 import Pokedex from '../pages/Pokedex';
 import pokemonList from '../data';
 
@@ -18,10 +19,7 @@ const favorites = {
 };
 
 test('Verifica se a página contém um texto "Encountered Pokémon"', () => {
-  renderWithRouter(<Pokedex
-    pokemonList={ pokemonList }
-    isPokemonFavoriteById={ favorites }
-  />);
+  renderWithRouter(<App />);
   screen.getByRole('heading', {
     name: /encountered pokémon/i,
   });
@@ -43,9 +41,55 @@ test('Verifica se é exibido o botão "Próximo Pokémon" e ao clicar nele, os p
 });
 
 test('Verifica se é mostrado apenas um pokemon por vez', () => {
-  
+  renderWithRouter(<Pokedex
+    pokemonList={ pokemonList }
+    isPokemonFavoriteById={ favorites }
+  />);
+  const pokemonElements = screen.getAllByTestId('pokemon-name');
+  expect(pokemonElements).toHaveLength(1);
 });
 
-test('', () => {});
+test('Verificando se a pokedex tem os botões de filtro', () => {
+  renderWithRouter(<Pokedex
+    pokemonList={ pokemonList }
+    isPokemonFavoriteById={ favorites }
+  />);
+  const buttonFilter = screen.getAllByTestId('pokemon-type-button');
+  pokemonList.forEach((pokemon) => {
+    screen.getByRole('button', { name: pokemon.type });
+  });
+  screen.getByRole('button', { name: /all/i });
+  expect(buttonFilter).toHaveLength(7);
+});
 
-test('', () => {});
+test('Verifica se a partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos Pokémon daquele tipo', () => {
+  renderWithRouter(<Pokedex
+    pokemonList={ pokemonList }
+    isPokemonFavoriteById={ favorites }
+  />);
+  const buttonFilter = screen.getByRole('button', { name: /fire/i });
+  const buttonNext = screen.getByRole('button', { name: /próximo pokémon/i });
+
+  userEvent.click(buttonFilter);
+  screen.getByRole('button', { name: /all/i });
+  screen.getByText(/charmander/i);
+  userEvent.click(buttonNext);
+  screen.getByRole('button', { name: /all/i });
+  screen.getByText(/rapidash/i);
+  userEvent.click(buttonNext);
+  screen.getByRole('button', { name: /all/i });
+  screen.getByText(/charmander/i);
+});
+
+test('Verificar se a pokedex possui o botão "All" para resetar o filtro', () => {
+  renderWithRouter(<Pokedex
+    pokemonList={ pokemonList }
+    isPokemonFavoriteById={ favorites }
+  />);
+  const resetFilter = screen.getByRole('button', { name: /all/i });
+  const buttonNext = screen.getByRole('button', { name: /próximo pokémon/i });
+  userEvent.click(buttonNext);
+  screen.getByText(/charmander/i);
+  userEvent.click(resetFilter);
+  screen.getByText(/pikachu/i);
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemonList from '../data';
+
+test('Verifica se é renderizado corretamente um card com as informações do pokemon', () => {
+  renderWithRouter(<Pokemon
+    pokemonList={ pokemonList }
+  />);
+  pokemonList.forEach((pokemon) => {
+
+  });
+});
+
+test('', () => {});
+
+test('', () => {});
+
 test('', () => {});
