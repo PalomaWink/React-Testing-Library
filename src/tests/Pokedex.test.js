0b428fc2e9b50367a8f64465913c2196729ed4d1
Paloma Wink <paloma.wink@hotmail.com>
2023-05-02 21:12:52 -0300

    Inicio do requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,51 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../pages/Pokedex';
+import pokemonList from '../data';
+
+const favorites = {
+  4: true,
+  10: false,
+  23: false,
+  25: true,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+test('Verifica se a página contém um texto "Encountered Pokémon"', () => {
+  renderWithRouter(<Pokedex
+    pokemonList={ pokemonList }
+    isPokemonFavoriteById={ favorites }
+  />);
+  screen.getByRole('heading', {
+    name: /encountered pokémon/i,
+  });
+});
+
+test('Verifica se é exibido o botão "Próximo Pokémon" e ao clicar nele, os próximos pokemons são exibidos na sequencia', () => {
+  renderWithRouter(<Pokedex
+    pokemonList={ pokemonList }
+    isPokemonFavoriteById={ favorites }
+  />);
+  const button = screen.getByRole('button', {
+    name: /próximo pokémon/i,
+  });
+  pokemonList.forEach((pokemon) => {
+    screen.getByText(pokemon.name);
+    userEvent.click(button, 'Próximo Pokémon');
+  });
+  screen.getByText(/pikachu/i);
+});
+
+test('Verifica se é mostrado apenas um pokemon por vez', () => {
+  
+});
+
+test('', () => {});
+
 test('', () => {});
